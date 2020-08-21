c5d2c8bf09ea0478f44dbfa1c9d81213340f2b3a
TanselArif-21 <tanselarif21@gmail.com>
2020-08-21 12:21:54 +0100

    Getting to 0.77751 with sex, pclass and cabin exists

--- Code/transformation.py
+++ Code/transformation.py
@@ -43,8 +43,8 @@ dic_OrigMap = {'Mr': 'Mr', 'Mrs': 'Mrs', 'Miss': 'Miss', 'Master':'Master','Don'
 
 dic_Map = {'Mr': 'Mr', 'Mrs': 'Mrs', 'Miss': 'Miss', 'Master':'Master','Don':'Mr','Rev':'Rev',
     'Mme':'Mrs',
-    'Ms':'Ms','Major':'Mr', 'Lady':'Mrs', 'Sir':'Master', 'Mlle':'Miss', 
-    'Col':'Col', 'Capt':'Rev', 'Countess':'Mrs','Jonkheer':'Master', 'Dr':'Dr', 'Dona':'Mrs'}
+    'Ms':'Ms','Major':'Mr', 'Lady':'Mrs', 'Sir':'Sir', 'Mlle':'Miss', 
+    'Col':'Col', 'Capt':'Mr', 'Countess':'Mrs','Jonkheer':'Sir', 'Dr':'Dr', 'Dona':'Mrs'}
 
 
 #################################################
@@ -120,18 +120,56 @@ def setAge(row,meanAge):
     else:
         return int(Age)
         
+def setAge0(row,meanAge):
+    '''
+    This function is intended to be used via the apply() funtion and applies to rows in the format (Age,sex,Pclass).
+    This function replaces any null Age with the mean Age of the same sex and Pclass as given in the meanAge argument/dataframe.
+    '''
+    Age = row[0]
+    male = row[1]
+    Pclass = row[2]
+    if(pd.isnull(Age)):
+        return int(meanAge[(meanAge['Pclass'] == Pclass) & (meanAge['male'] == male)]['Age'].iloc[0])
+    else:
+        return int(Age)
+        
 def setFare(row,meanFare):
     '''
     This function is intended to be used via the apply() funtion and applies to rows in the format (Fare,sex,Pclass).
     This function replaces any null Fare with the mean Fare of the same sex and Pclass as given in the meanFare argument/dataframe.
     '''
     Fare = row[0]
+    
+    if pd.isna(Fare):
+        return 0
+    else:
+        return Fare
+    
     male = row[1]
     Pclass = row[2]
     TotalPpl = row[3]
     if(pd.isnull(Fare)  & (np.sum(meanFare['TotalPpl'] == TotalPpl) > 0)):
         return int(meanFare[(meanFare['Pclass'] == Pclass) & (meanFare['male'] == male) & (meanFare['TotalPpl'] == TotalPpl)]['Fare'].iloc[0])
     elif(pd.isnull(Fare)):
+        return int(meanFare[(meanFare['Pclass'] == Pclass) & (meanFare['male'] == male)]['Fare'].iloc[0])
+    else:
+        return int(Fare)
+        
+def setFare0(row,meanFare):
+    '''
+    This function is intended to be used via the apply() funtion and applies to rows in the format (Fare,sex,Pclass).
+    This function replaces any null Fare with the mean Fare of the same sex and Pclass as given in the meanFare argument/dataframe.
+    '''
+    Fare = row[0]
+    
+    if pd.isna(Fare):
+        return 0
+    else:
+        return Fare
+    
+    male = row[1]
+    Pclass = row[2]
+    if(pd.isnull(Fare)):
         return int(meanFare[(meanFare['Pclass'] == Pclass) & (meanFare['male'] == male)]['Fare'].iloc[0])
     else:
         return int(Fare)
@@ -162,6 +200,18 @@ def addCabinLength(df):
 def addTotalPpl(df):
     df['TotalPpl'] = df['Parch'] + df['SibSp']
     
+def addIsChild(df):
+    df['IsChild'] = df['Age'].apply(lambda x: 1 if x < 16 else 0)
+    
+def addIsOAP(df):
+    df['IsOAP'] = df['Age'].apply(lambda x: 1 if x >= 60 else 0)
+    
+def addIsZeroFare(df):
+    df['IsZeroFare'] = df['Fare'].apply(lambda x: 1 if x == 0 else 0)
+    
+def addCabinCount(df):
+    df['CabinCount'] = df['Cabin'].apply(lambda x: len(str(x).split()))
+    
 def makeColumnsEqual(df,df1):
     for col in df1.columns:
         if col not in df.columns:
@@ -171,6 +221,20 @@ def makeColumnsEqual(df,df1):
         if col not in df1.columns:
             print('Removed col {}'.format(col))
             df.drop(col,inplace=True,axis=1)
+           
+def addFare(df):
+    df['HasFare'] = df['Fare'].apply(lambda x: 0 if pd.isnull(x) else 1) 
+    
+def addFareBucket(df):
+    df['FareBucket'] = df['Fare'].apply(lambda x: 'Unknown' if pd.isnull(x) else '0' if x == 0 else '<50' if x < 50 else '<100' if x < 100 else '>100' if x >= 100 else 'Unknown')
+    fareBucket = pd.get_dummies(df['FareBucket'],drop_first=True,prefix='farebucket_')
+    df.drop(['FareBucket'],axis=1,inplace=True)
+    df = pd.concat([df,fareBucket],axis=1)
+    
+    return df
+    
+def addAge(df):
+    df['AgeCleaned'] = df['Age'].apply(lambda x: 0 if pd.isnull(x) else x)
     
     
 #################################################
@@ -184,33 +248,41 @@ def cleanTitanicData(df):
     
     #Sex is either male or female. Create a series which has entries 0 and 1 specifying whether it's one or the other
     sex = pd.get_dummies(df['Sex'],drop_first=True)
+
+    # PClass get dummies
+    pclass = pd.get_dummies(df['Pclass'],drop_first=True,prefix='pclass_')
+    
+    # Add Age
+    addAge(df)
     
     #Similar to the sex series
-    embark = pd.get_dummies(df['Embarked'],drop_first=True)
+    #embark = pd.get_dummies(df['Embarked'],drop_first=True)
 
     #Cabin either exists or doesn't -> 1 or 0
     addCabinExists(df)
     
+    # Get the total people
+    #addTotalPpl(df)
+    
     # Get the ticket number and then bucket them into >200000 and < 200000
-    addTicketBucket(df)
-    TicketBucket = pd.get_dummies(df['TicketBucket'],drop_first=True)
+    #addTicketBucket(df)
+    #TicketBucket = pd.get_dummies(df['TicketBucket'],drop_first=True)
     
     #Get the title from the name
-    addTitle(df)
-    df['Title'] = df['Title'].map(dic_Map)
-    Title =  pd.get_dummies(df['Title'],drop_first=True)
+    #addTitle(df)
+    #df['Title'] = df['Title'].map(dic_Map)
+    #Title =  pd.get_dummies(df['Title'],drop_first=True)
     
     # Get the name lenth and bucket it
-    addNameLength(df)
+    #addNameLength(df)
+    
+    #addCabinCount(df)
     
     #Get rid of some columns
-    df_temp = df.drop(['Sex','Embarked','Name','Ticket','Cabin','Title','TicketBucket'],axis=1,inplace=False)
+    df_temp = df.drop(['Sex','Embarked','Name','Ticket','Cabin','Age','Pclass', 'SibSp', 'Parch','Fare'],axis=1,inplace=False)
     
     #Concat the series to the data frame
-    df_temp = pd.concat([df_temp,sex,embark,Title,TicketBucket],axis=1)
-    
-    # Get the total people
-    addTotalPpl(df_temp)
+    df_temp = pd.concat([df_temp,sex,pclass],axis=1)
     
     return df_temp
     
@@ -226,21 +298,28 @@ def fillTitanicNa(df,trainingData,fillAge=True,fillFare=True):
     df_train = cleanTitanicData(trainingData)
 
     # Get the mean Fare values per grouping
-    t_Fare = pd.DataFrame(df_train.dropna().groupby(['Pclass','male','TotalPpl']).median()['Fare'])
-    t_Fare = pd.DataFrame(t_Fare.to_records())
+    #t_Fare = pd.DataFrame(df_train.dropna().groupby(['Pclass','male','TotalPpl']).mean()['Fare'])
+    #t_Fare = pd.DataFrame(t_Fare.to_records())
 
     # Get the mean Age values per grouping
-    t_Age = pd.DataFrame(df_train.dropna().groupby(['Pclass','male','TotalPpl']).median()['Age'])
-    t_Age = pd.DataFrame(t_Age.to_records())
+    #t_Age = pd.DataFrame(df_train.dropna().groupby(['Pclass','male','TotalPpl']).mean()['Age'])
+    #t_Age = pd.DataFrame(t_Age.to_records())
     
-    if(fillAge == True):
-        df_temp['Age'] = df_temp[['Age','male','Pclass','TotalPpl']].apply(lambda row: setAge(row,t_Age),axis=1)
+    
+    #if(fillAge == True):
+        #df_temp['Age'] = df_temp[['Age','male','Pclass','TotalPpl']].apply(lambda row: setAge(row,t_Age),axis=1)
+        #df_temp['Age'] = df_temp[['Age','male']].apply(lambda row: setAge0(row,t_Age),axis=1)
 
-    if(fillFare == True):
-        df_temp['Fare'] = df_temp[['Fare','male','Pclass','TotalPpl']].apply(lambda row: setFare(row,t_Fare),axis=1)
+    #if(fillFare == True):
+        #df_temp['Fare'] = df_temp[['Fare','male','Pclass','TotalPpl']].apply(lambda row: setFare(row,t_Fare),axis=1)
+        #df_temp['Fare'] = df_temp[['Fare','male']].apply(lambda row: setFare0(row,t_Fare),axis=1)
         
-    if(fillAge == False or fillFare == False):
-        df_temp.dropna(inplace = True)
+    #if(fillAge == False or fillFare == False):
+    #    df_temp.dropna(inplace = True)
+        
+    #addIsChild(df_temp)
+    #addIsOAP(df_temp)
+    #addIsZeroFare(df_temp)
         
     return df_temp
     
